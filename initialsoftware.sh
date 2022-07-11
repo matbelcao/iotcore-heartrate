63d296df707e8aac2fcb7cd572c02f98b53b9e11
Matteo Belcao <37814058+matbelcao@users.noreply.github.com>
2022-07-11 19:04:10 +0200

    Update initialsoftware.sh

--- initialsoftware.sh
+++ initialsoftware.sh
@@ -13,5 +13,5 @@
 # limitations under the License.
 
 sudo apt-get update
-sudo apt-get -y install build-essential libssl-dev python-dev libffi-dev python-pip git
+sudo apt-get -y install build-essential libssl-dev python-dev libffi-dev python3-pip git
 sudo pip install -r requirements.txt
